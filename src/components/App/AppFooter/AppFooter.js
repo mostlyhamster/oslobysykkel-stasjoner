318987531a48292312bcc3909c872baba0946986
Riv <rivian@protonmail.com>
2020-03-01 16:51:16 +0100

    Add "back to top" function

--- src/components/App/AppFooter/AppFooter.js
+++ src/components/App/AppFooter/AppFooter.js
@@ -3,9 +3,15 @@ import React from 'react';
 import './AppFooter.css';
 
 const AppFooter = (props) => {
+    const backToTop = () => {
+        console.log("top")
+        window.scrollTo( 0,0);
+    }
     return (
         <div className="app__footer">
-            <p>Laget av Roger Wlosek for Oslo Origo</p>
+            <p><button onClick={backToTop} className="app-footer__button--normal">Tilbake til toppen</button></p>
+            <br></br>
+            <p>Laget av Roger Wlosek for Oslo Origo </p>
         </div>
     );
 };
