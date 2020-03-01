bbafb416c47bc69e0be9ce0bdfb593ccdb22e9d7
Riv <rivian@protonmail.com>
2020-03-01 10:43:21 +0100

    Sette opp prosjekt

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="app__container">
+      <div className="app__header">
+        <h1>Stasjoner og låser</h1>
+      </div>
+        content
+    </div>
+  );
+}
+
+export default App;
