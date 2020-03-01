16e055b0ba22c4e42fea31ad5668318bec9c1e1d
Riv <rivian@protonmail.com>
2020-03-01 11:26:38 +0100

    Add Header

---
+++ src/components/App/AppHeader/AppHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppHeader = (props) => {
+    return (
+        <header className="app__header">
+            <h1>Oversikt over bysykkel stasjoner og låser</h1>
+        </header>
+    );
+};
+
+export default AppHeader;
